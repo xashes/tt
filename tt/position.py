a9965c62d276b1a1647e891f2cc51aa1b375a6c6
xashes <xashes@outlook.com>
2017-02-28 01:04:45 +0800

    format

--- tt/position.py
+++ tt/position.py
@@ -2,7 +2,16 @@ class Position:
 
     def __init__(self, sid):
         self.sid = sid
+        self.quantity = 0
+        self.bought_quantity = 0
+        self.sold_quantity = 0
+        self.bought_value = 0
+        self.sold_value = 0
+        self.total_trades = 0
+        self.sellable = 0
+        self.average_cost = 0
+        self.market_value = 0
+        self.value_percent = 0
 
-
-
-
+    def __repr__(self):
+        return "Position({})".format(self.__dict__)
