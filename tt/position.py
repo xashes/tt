a1e0ca69be6400ff8e60b0e7e795ca77c2ad7f45
xashes <xashes@outlook.com>
2017-02-27 23:26:03 +0800

    delete useless comments

--- tt/position.py
+++ tt/position.py
@@ -1,16 +1,8 @@
-"""
-Position object
-"""
-
 class Position:
-    """
-    To represent position status for a security
-    """
+
     def __init__(self, sid):
         self.sid = sid
 
-    def record(self):
-        """
-        Return record DataFrame
-        """
-        pass
+
+
+
